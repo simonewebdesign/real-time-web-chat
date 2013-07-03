1a3bff6b4d33b36a74af137051543cd6c9698ebb
Simone <hello@simonewebdesign.it>
2013-07-03 17:04:06 -0400

    Use classes instead of ids, and querySelector() instead of getElementById()

--- public/chat.js
+++ public/chat.js
@@ -2,9 +2,9 @@ window.onload = function() {
 
     var messages = [];
     var socket = io.connect('http://localhost:3700');
-    var field = document.getElementById("field");
-    var sendButton = document.getElementById("send");
-    var content = document.getElementById("content");
+    var field = document.querySelector('.field');
+    var sendButton = document.querySelector('.send');
+    var content = document.querySelector('.content');
 
     socket.on('message', function (data) {
         if(data.message) {
@@ -15,7 +15,7 @@ window.onload = function() {
             }
             content.innerHTML = html;
         } else {
-            console.log("There is a problem:", data);
+            console.log("Houston, we have a problem: ", data);
         }
     });
 
