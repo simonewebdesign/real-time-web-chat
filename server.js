7b11c42db884d7b49ff1de1b6ed91ac106fd841c
Simone <hello@simonewebdesign.it>
2013-08-26 17:56:11 -0400

    Clean server

--- server.js
+++ server.js
@@ -1,43 +1,15 @@
-
 /***** SERVER *****/
 
 var express = require("express"),
     app = express(),
     port = 1337;
 
-// This is how we use external libs
-//var _ = require("underscore");
-//var $ = require('jquery');
-
-// Just a test route (Express)
-//app.get("/", function(req, res){
-//    res.send("It works!");
-//});
-
 app.use(express.static(__dirname + '/public'));
 
-// app.listen(port);
 // We pass the express server to socket.io
 var io = require('socket.io').listen(app.listen(port));
 
-///////////////////////////////////////////////////
-
-// Socket.io connection handler
-io.sockets.on('connection', function (socket) {
-
-    //console.log(socket);
-    //console.log(window); this is of course not available in the server's side
-
-    // socket is the client's socket,
-    // the junction between the server and the user's browser.
-
-    // This is the welcome message, from Server:
-    //socket.emit('message sent', {
-    //    name: 'Server',
-    //    text: 'welcome to the chat!',
-    //    type: 0,
-    //    time: (new Date()).getTime()
-    //});
+io.sockets.on('connection', function (socket) { // socket is the client's socket, the junction between the server and the user's browser.
 
     socket.emit('connected', { id: socket.id });
 
@@ -65,17 +37,13 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('send message', function (data) {
         // forward the data sent by the user to all other sockets
-        //console.log("MESSAGE SENT: " + JSON.stringify(data));
         io.sockets.emit('message sent', data);
     });
 
     socket.on('disconnect', function () {
 
         socket.get('nickname', function(err, name) {
-            socket.broadcast.emit('disconnected', { 
-                //id: this.id,
-                name: name
-            });
+            socket.broadcast.emit('disconnected', { name: name });
         });
     });
 });
@@ -94,16 +62,17 @@ app.get('/', function(req, res){
     res.render('page'); // page.jade is our template
 });
 
-// test route
-app.get('/hello.txt', function(req, res){
-  var body = 'Hello World';
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', body.length);
+// test route (Express)
+//app.get('/hello.txt', function(req, res){
+//  var body = 'Hello World';
+//  res.setHeader('Content-Type', 'text/plain');
+//  res.setHeader('Content-Length', body.length);
+//  var foo = require('./public/foo.json');
+//  console.log(foo);
+//  res.end(body);
+//});
 
-  var foo = require('./public/foo.json');
-  console.log(foo);
-
-  res.end(body);
-});
-
-
+// another test route
+//app.get("/", function(req, res){
+//    res.send("It works!");
+//});
