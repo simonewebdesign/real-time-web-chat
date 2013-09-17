4ba2d079e7819aa0ece5dd014d7c80f2f683bdad
simonewebdesign <hello@simonewebdesign.it>
2013-09-17 16:08:46 -0400

    fix connection string

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ var SampleApp = function() {
         //load the Client interface
         var MongoClient = require('mongodb').MongoClient;
         // the client db connection scope is wrapped in a callback:
-        MongoClient.connect('mongodb://'+connection_string, function(err, db) {
+        MongoClient.connect('mongodb://' + self.connection_string, function(err, db) {
           if(err) throw err;
           var collection = db.collection('messages').find().limit(10).toArray(function(err, docs) {
             console.log(docs);
