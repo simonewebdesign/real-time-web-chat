3f8c35f3b0f7a332ceb7e13cf6a118175de0fd07
simonewebdesign <hello@simonewebdesign.it>
2013-09-28 05:38:24 -0400

    Refactoring Timer

--- public/timer.js
+++ public/timer.js
@@ -5,33 +5,31 @@ define([], function() {
   // object   options
   var Timer = function(callback, options) {
 
-    if (!options) {
-      options = {};
-    }
+    var self = this, 
+        options = options || {};
 
-    this.delay = options.delay ? options.delay : 3000;
+    this.delay = options.delay || 3000;
     this.isRunning = false;
 
     this.start = function() {
 
       this.isRunning = true;
 
-      var that = this;
       setTimeout(function() {
-        that.onFinish();
+        self.onFinish();
       }, this.delay);
-    };
+    }
 
     this.reset = function() {
       clearTimeout(this);
-    };
+    }
 
     this.onFinish = function() {
       if (typeof callback == 'function') {
         callback();
       }
       this.isRunning = false;
-    };
+    }
   }
 
   return Timer;
